cdb309f7734bd1008f1bc13acc1b59d53ca7b299
Henrique <henrique.roco@gmail.com>
2015-07-18 05:22:32 -0300

    Hibernate

--- src/main/java/br/lumi/server/ClienteModel.java
+++ src/main/java/br/lumi/server/ClienteModel.java
@@ -1,11 +1,11 @@
 package br.lumi.server;
 
+import javax.persistence.Table;
+
 import org.hibernate.annotations.Entity;
-import org.hibernate.annotations.Table;
-import org.hibernate.annotations.Tables;
 
 @Entity
-@Tables(name = "cliente")
+@Table(name = "cliente")
 public class ClienteModel {
 
 	
