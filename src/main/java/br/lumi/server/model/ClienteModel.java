9d9f940e033072540e3641312436ca81965c8357
Henrique Roco Weiber <henriqueroco@softpharma.com.br>
2015-07-23 13:12:10 -0300

    modelos

--- src/main/java/br/lumi/server/model/ClienteModel.java
+++ src/main/java/br/lumi/server/model/ClienteModel.java
@@ -1,5 +1,8 @@
 package br.lumi.server.model;
 
+/**
+ * @author WEIBER
+ */
 import java.util.Date;
 
 import javax.persistence.CascadeType;
@@ -16,6 +19,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
 
 import br.lumi.server.enuns.EEstadoCivil;
+import br.lumi.server.enuns.ESexo;
 
 @Entity
 @Table(name = ClienteModel.TABELA)
@@ -25,6 +29,16 @@ public class ClienteModel {
 	static final String ATRIBUTO_ID = "cpf_id";
 	private static final String SEQUENCE_ID = "seq_id_cliente";
 	private static final String ATRIBUTO_DATA_CADASTRO = "data_cadastro";
+	private static final String ATRIBUTO_NOME = "nome";
+	private static final String ATRIBUTO_DATA_NACIMENTO = "data_nacimento";
+	private static final String ATRIBUTO_IDADE = "idade";
+	private static final String ATRIBUTO_SEXO = "sexo";
+	private static final String ATRIBUTO_TELEFONE = "telefone";
+	private static final String ATRIBUTO_CELULAR = "celular";
+	private static final String ATRIBUTO_OCUPACAO = "ocupacao";
+	private static final String ATRIBUTO_ESTADO_CIVIL = "estado_civil";
+	private static final String ATRIBUTO_NOME_AVISO_EMERGENCIA = "avisar";
+	private static final String ATRIBUTO_TELEFONE_EMERGENCIA = "telefone_avisar";
 	
 	@Id
 	@GeneratedValue(generator = ClienteModel.SEQUENCE_ID)
@@ -36,31 +50,145 @@ public class ClienteModel {
 	@Column(name = ATRIBUTO_ID, nullable = false, unique = true)
 	String cpf;
 	
+	@Column(name = ATRIBUTO_NOME, nullable = false)
 	String nome;
 	
-	Date dataNascimento;
+	@Temporal(TemporalType.DATE)
+	@Column(name = ATRIBUTO_DATA_NACIMENTO, nullable = false)
+	Date dataNacimento;
 	
+	@Column(name = ATRIBUTO_IDADE, nullable = false)
 	Integer idade;
 	
-	Integer sexo;
+	@Column(name = ATRIBUTO_SEXO, nullable = false)
+	ESexo sexo;
 	
 	@Temporal(TemporalType.DATE)
 	@Column(name = ATRIBUTO_DATA_CADASTRO, nullable = false)
 	Date dataCadastro;
 	
+	@Column(name = ATRIBUTO_TELEFONE)
 	String telefone;
 	
+	@Column(name = ATRIBUTO_CELULAR, nullable = false)
 	String celular;
+	
+	@Column(name = ATRIBUTO_OCUPACAO)
+	String ocupacao;
+	
+	@Column(name = ATRIBUTO_ESTADO_CIVIL, nullable = false)
+	EEstadoCivil estadoCivil;
+	
+	@Column(name = ATRIBUTO_NOME_AVISO_EMERGENCIA)
+	String nomeAvisoEmergencia;
+	
+	@Column(name = ATRIBUTO_TELEFONE_EMERGENCIA)
+	String telefoneEmergencia;
 	
 	@OneToOne(mappedBy = "convenio", cascade = CascadeType.ALL, orphanRemoval = true)
 	LogradouroModel logradouro;
-	
-	String ocupacao;
-	
-	EEstadoCivil estadoCivil;
-	
-	String nomeAvisoEmergencia;
-	
-	String telefoneEmergencia;
-	
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Date getDataNacimento() {
+		return dataNacimento;
+	}
+
+	public void setDataNacimento(Date dataNacimento) {
+		this.dataNacimento = dataNacimento;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	public ESexo getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(ESexo sexo) {
+		this.sexo = sexo;
+	}
+
+	public Date getDataCadastro() {
+		return dataCadastro;
+	}
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getCelular() {
+		return celular;
+	}
+
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+
+	public String getOcupacao() {
+		return ocupacao;
+	}
+
+	public void setOcupacao(String ocupacao) {
+		this.ocupacao = ocupacao;
+	}
+
+	public EEstadoCivil getEstadoCivil() {
+		return estadoCivil;
+	}
+
+	public void setEstadoCivil(EEstadoCivil estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+
+	public String getNomeAvisoEmergencia() {
+		return nomeAvisoEmergencia;
+	}
+
+	public void setNomeAvisoEmergencia(String nomeAvisoEmergencia) {
+		this.nomeAvisoEmergencia = nomeAvisoEmergencia;
+	}
+
+	public String getTelefoneEmergencia() {
+		return telefoneEmergencia;
+	}
+
+	public void setTelefoneEmergencia(String telefoneEmergencia) {
+		this.telefoneEmergencia = telefoneEmergencia;
+	}
+
+	public LogradouroModel getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(LogradouroModel logradouro) {
+		this.logradouro = logradouro;
+	}
 }
--- src/main/java/br/lumi/server/model/LogradouroModel.java
+++ src/main/java/br/lumi/server/model/LogradouroModel.java
@@ -1,18 +1,53 @@
 package br.lumi.server.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+
+@Entity
+@Table(name = LogradouroModel.TABELA)
 public class LogradouroModel {
 
-	private static final String ATRIBUTO_ID = "id";
+	static final String TABELA = "logradouro";
+	static final String ATRIBUTO_ID = "id";
+	private static final String SEQUENCE_ID = "seq_id_logradouro";
+	private static final String ATRIBUTO_CEP = "";
+	private static final String ATRIBUTO_ENDERECO = "";
+	private static final String ATRIBUTO_NUMERO = "";
+	private static final String ATRIBUTO_COMPLEMENTO = "";
+	private static final String ATRIBUTO_BAIRRO = "";
 	
+	@Id
+	@GeneratedValue(generator = LogradouroModel.SEQUENCE_ID)
+	@GenericGenerator(name = LogradouroModel.SEQUENCE_ID, 
+		strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator", parameters = {
+			@Parameter(name = "sequence_name", value = LogradouroModel.SEQUENCE_ID),
+			@Parameter(name = "optimizer", value = "hilo"),
+			@Parameter(name = "increment_size", value = "1") })
+	@Column(name = ATRIBUTO_ID, nullable = false, unique = true)
+	Integer id;
+	
+	@Column(name = ATRIBUTO_CEP, nullable = false)
 	String cep;
+	
+	@Column(name = ATRIBUTO_ENDERECO)
 	String endereco;
+	
+	@Column(name = ATRIBUTO_NUMERO)
 	Integer numero;
+	
+	@Column(name = ATRIBUTO_COMPLEMENTO)
 	String complemento;
-	CidadeModel cidade;
+	
+	@Column(name = ATRIBUTO_BAIRRO)
 	String bairro;
 	
 	@OneToOne(optional = false)
@@ -20,4 +55,6 @@ public class LogradouroModel {
 	@MapsId("idCliente")
 	ClienteModel clietne;
 	
+//	@Column(name = ATRIBUTO_
+	CidadeModel cidade;
 }
